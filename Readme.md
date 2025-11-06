079ab276b2df54b45f880692d544430c9f19a3be
victor <victorsbn3@gmail.com>
2025-11-05 23:35:03 -0300

    agora printa 10vzs o

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 teste para aprender a usar o git
+[vai tomar no cu pedro se fode se mata eu te odeio]
--- teste.c
+++ teste.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello, world");
+    for (int i = 0; i < 10; i++) {
+        printf("hello, world\n");
+    }
     return 0;
-}
+} 
